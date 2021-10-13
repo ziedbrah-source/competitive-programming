fc2369b2c677bc7ab80f68f382deec35a116a7a2
Mohamed zied Brahmi <68474414+ziedbrah-source@users.noreply.github.com>
2021-10-13 18:03:49 +0100

    leetcode remove duplicates from sorted array , v1 (slow)

---
+++ leetcode/Remove Duplicates from sorted array v1.cpp
@@ -0,0 +1,32 @@
+
+#include <iostream>
+
+using namespace std; 
+#include <vector>
+#include <unordered_map>
+using namespace std;
+int removeDuplicates(vector<int>& nums) {
+        int max=2147483647;
+        int j;
+        int i=0;
+        int length=nums.size();
+        while(i<nums.size()){
+            int j;
+            for(j=i+1;j<nums.size();j++){
+                if(nums[i]==nums[j]){
+                    nums[j]=max;
+                    length--;
+                }else {
+                    break;
+                }
+            }
+            i=j;
+        }
+        sort(nums.begin(),nums.end());
+        return length;
+    }
+    
+    int main(){
+	// bla bla , initiate a vector, target and test.
+	return 0;
+}
